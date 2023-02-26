add11beb41648d1968d47c401b0a9cab09da3be9
AtakanKorez <atakankorez@gmail.com>
2023-02-26 21:32:13 +0100

    Adding NgxSpinner and BaseComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,19 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private toastr:ToastrService) { }
+  constructor(private spinner: NgxSpinnerService) { }
 
   title = 'ECommerceClient';
 
   ngOnInit(): void {
-    this.toastr.success("Atakan","Baslik");
+    //this.toastr.success("Atakan", "Baslik");
+    /** spinner starts on init */
+    this.spinner.show("s2");
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide("s2");
+    }, 5000);
   }
 
 }
